848e7dbf399ff09f9b5cc050c9106ed8a9de360e
Yuki Hirayama <yhirayama20188.itn@gmail.com>
2019-04-08 00:10:09 +0900

    190407-190408

---
+++ dist/js/mapList.js
@@ -0,0 +1,527 @@
+var mapList = [
+  {
+    "dst_name": "アジア",
+    "dst_code": "ASI",
+    "country_name": "韓国",
+    "country_code": "KOR",
+    "city_name": "ソウル",
+    "city_code": "SEL",
+    "マップコード": "ASI"
+  },
+  {
+    "dst_name": "アジア",
+    "dst_code": "ASI",
+    "country_name": "韓国",
+    "country_code": "KOR",
+    "city_name": "プサン",
+    "city_code": "PUS",
+    "マップコード": "ASI"
+  },
+  {
+    "dst_name": "アジア",
+    "dst_code": "ASI",
+    "country_name": "中国",
+    "country_code": "CHN",
+    "city_name": "北京",
+    "city_code": "PEK",
+    "マップコード": "ASI"
+  },
+  {
+    "dst_name": "アジア",
+    "dst_code": "ASI",
+    "country_name": "中国",
+    "country_code": "CHN",
+    "city_name": "上海",
+    "city_code": "SHA",
+    "マップコード": "ASI"
+  },
+  {
+    "dst_name": "アジア",
+    "dst_code": "ASI",
+    "country_name": "フィリピン",
+    "country_code": "PHL",
+    "city_name": "マニラ",
+    "city_code": "MNL",
+    "マップコード": "ASI"
+  },
+  {
+    "dst_name": "アジア",
+    "dst_code": "ASI",
+    "country_name": "マレーシア",
+    "country_code": "MYS",
+    "city_name": "クアラルンプール",
+    "city_code": "KUL",
+    "マップコード": "ASI"
+  },
+  {
+    "dst_name": "アジアビーチ",
+    "dst_code": "ASI",
+    "country_name": "インドネシア",
+    "country_code": "IDN",
+    "city_name": "バリ島",
+    "city_code": "DPS",
+    "マップコード": "ASB"
+  },
+  {
+    "dst_name": "アジアビーチ",
+    "dst_code": "ASI",
+    "country_name": "フィリピン",
+    "country_code": "PHL",
+    "city_name": "セブ島",
+    "city_code": "CEB",
+    "マップコード": "ASB"
+  },
+  {
+    "dst_name": "アジアビーチ",
+    "dst_code": "ASI",
+    "country_name": "マレーシア",
+    "country_code": "MYS",
+    "city_name": "コタキナバル",
+    "city_code": "BKI",
+    "マップコード": "ASB"
+  },
+  {
+    "dst_name": "アジアビーチ",
+    "dst_code": "ASI",
+    "country_name": "マレーシア",
+    "country_code": "MYS",
+    "city_name": "ペナン島",
+    "city_code": "PEN",
+    "マップコード": "ASB"
+  },
+  {
+    "dst_name": "アジアビーチ",
+    "dst_code": "ASI",
+    "country_name": "マレーシア",
+    "country_code": "MYS",
+    "city_name": "ランカウイ島",
+    "city_code": "LKA",
+    "マップコード": "ASB"
+  },
+  {
+    "dst_name": "ハワイ",
+    "dst_code": "HGS",
+    "country_name": "ハワイ",
+    "country_code": "HWI",
+    "city_name": "ホノルル",
+    "city_code": "HNL",
+    "マップコード": "HWI"
+  },
+  {
+    "dst_name": "ハワイ",
+    "dst_code": "HGS",
+    "country_name": "ハワイ",
+    "country_code": "HWI",
+    "city_name": "ハワイ島",
+    "city_code": "KOA",
+    "マップコード": "HWI"
+  },
+  {
+    "dst_name": "ハワイ",
+    "dst_code": "HGS",
+    "country_name": "ハワイ",
+    "country_code": "HWI",
+    "city_name": "カウアイ島",
+    "city_code": "JIH",
+    "マップコード": "HWI"
+  },
+  {
+    "dst_name": "ハワイ",
+    "dst_code": "HGS",
+    "country_name": "ハワイ",
+    "country_code": "HWI",
+    "city_name": "ラナイ島",
+    "city_code": "LNY",
+    "マップコード": "HWI"
+  },
+  {
+    "dst_name": "グアム・サイパン",
+    "dst_code": "HGS",
+    "country_name": "グアム",
+    "country_code": "GUM",
+    "city_name": "グアム",
+    "city_code": "GUM"
+  },
+  {
+    "dst_name": "グアム・サイパン",
+    "dst_code": "HGS",
+    "country_name": "サイパン",
+    "country_code": "SPN",
+    "city_name": "サイパン",
+    "city_code": "SPN"
+  },
+  {
+    "dst_name": "ヨーロッパ",
+    "dst_code": "EUR",
+    "country_name": "フランス",
+    "country_code": "FRA",
+    "city_name": "パリ",
+    "city_code": "PAR",
+    "マップコード": "EUR"
+  },
+  {
+    "dst_name": "ヨーロッパ",
+    "dst_code": "EUR",
+    "country_name": "フランス",
+    "country_code": "FRA",
+    "city_name": "マルセイユ",
+    "city_code": "MRS",
+    "マップコード": "EUR"
+  },
+  {
+    "dst_name": "ヨーロッパ",
+    "dst_code": "EUR",
+    "country_name": "イタリア",
+    "country_code": "ITA",
+    "city_name": "ローマ",
+    "city_code": "ROM",
+    "マップコード": "EUR"
+  },
+  {
+    "dst_name": "ヨーロッパ",
+    "dst_code": "EUR",
+    "country_name": "イタリア",
+    "country_code": "ITA",
+    "city_name": "フィレンツェ",
+    "city_code": "FLR",
+    "マップコード": "EUR"
+  },
+  {
+    "dst_name": "中近東・アフリカ",
+    "dst_code": "AFR",
+    "country_name": "モロッコ",
+    "country_code": "MAR",
+    "city_name": "カサブランカ",
+    "city_code": "CAS",
+    "マップコード": "AFR"
+  },
+  {
+    "dst_name": "南太平洋・オセアニア",
+    "dst_code": "OCE",
+    "country_name": "オーストラリア",
+    "country_code": "AUS",
+    "city_name": "ケアンズ",
+    "city_code": "CNS",
+    "マップコード": "OCE"
+  },
+  {
+    "dst_name": "南太平洋・オセアニア",
+    "dst_code": "OCE",
+    "country_name": "ニュージーランド",
+    "country_code": "NZL",
+    "city_name": "クライストチャーチ",
+    "city_code": "CHC",
+    "マップコード": "OCE"
+  },
+  {
+    "dst_name": "カナダ",
+    "dst_code": "CAN",
+    "country_name": "カナダ",
+    "country_code": "CAN",
+    "city_name": "イエローナイフ",
+    "city_code": "YZF",
+    "マップコード": "CAN"
+  },
+  {
+    "dst_name": "アメリカ",
+    "dst_code": "USA",
+    "country_name": "アメリカ合衆国",
+    "country_code": "USA",
+    "city_name": "ニューヨーク",
+    "city_code": "NYC",
+    "マップコード": "USA"
+  },
+  {
+    "dst_name": "中南米・カリブ海",
+    "dst_code": "MEX",
+    "country_name": "メキシコ",
+    "country_code": "MEX",
+    "city_name": "メキシコシティ",
+    "city_code": "MWX",
+    "マップコード": "MEX"
+  },
+  {
+    "dst_name": "中南米・カリブ海",
+    "dst_code": "MEX",
+    "country_name": "メキシコ",
+    "country_code": "MEX",
+    "city_name": "カンクン",
+    "city_code": "CUN",
+    "マップコード": "MEX"
+  }
+];
+var mex = mapList.filter(function (item, index) {
+  if (item.dst_code == 'MEX') return true;
+});
+console.log(mex);
+
+var sortByDst = {
+  "ASI": [
+    {
+      "dst_name": "アジア",
+      "dst_code": "ASI",
+      "country_name": "韓国",
+      "country_code": "KOR",
+      "city_name": "ソウル",
+      "city_code": "SEL",
+      "マップコード": "ASI"
+    },
+    {
+      "dst_name": "アジア",
+      "dst_code": "ASI",
+      "country_name": "韓国",
+      "country_code": "KOR",
+      "city_name": "プサン",
+      "city_code": "PUS",
+      "マップコード": "ASI"
+    },
+    {
+      "dst_name": "アジア",
+      "dst_code": "ASI",
+      "country_name": "中国",
+      "country_code": "CHN",
+      "city_name": "北京",
+      "city_code": "PEK",
+      "マップコード": "ASI"
+    },
+    {
+      "dst_name": "アジア",
+      "dst_code": "ASI",
+      "country_name": "中国",
+      "country_code": "CHN",
+      "city_name": "上海",
+      "city_code": "SHA",
+      "マップコード": "ASI"
+    },
+    {
+      "dst_name": "アジア",
+      "dst_code": "ASI",
+      "country_name": "フィリピン",
+      "country_code": "PHL",
+      "city_name": "マニラ",
+      "city_code": "MNL",
+      "マップコード": "ASI"
+    },
+    {
+      "dst_name": "アジア",
+      "dst_code": "ASI",
+      "country_name": "マレーシア",
+      "country_code": "MYS",
+      "city_name": "クアラルンプール",
+      "city_code": "KUL",
+      "マップコード": "ASI"
+    },
+    {
+      "dst_name": "アジアビーチ",
+      "dst_code": "ASI",
+      "country_name": "インドネシア",
+      "country_code": "IDN",
+      "city_name": "バリ島",
+      "city_code": "DPS",
+      "マップコード": "ASB"
+    },
+    {
+      "dst_name": "アジアビーチ",
+      "dst_code": "ASI",
+      "country_name": "フィリピン",
+      "country_code": "PHL",
+      "city_name": "セブ島",
+      "city_code": "CEB",
+      "マップコード": "ASB"
+    },
+    {
+      "dst_name": "アジアビーチ",
+      "dst_code": "ASI",
+      "country_name": "マレーシア",
+      "country_code": "MYS",
+      "city_name": "コタキナバル",
+      "city_code": "BKI",
+      "マップコード": "ASB"
+    },
+    {
+      "dst_name": "アジアビーチ",
+      "dst_code": "ASI",
+      "country_name": "マレーシア",
+      "country_code": "MYS",
+      "city_name": "ペナン島",
+      "city_code": "PEN",
+      "マップコード": "ASB"
+    },
+    {
+      "dst_name": "アジアビーチ",
+      "dst_code": "ASI",
+      "country_name": "マレーシア",
+      "country_code": "MYS",
+      "city_name": "ランカウイ島",
+      "city_code": "LKA",
+      "マップコード": "ASB"
+    }
+  ],
+  "HGS": [
+    {
+      "dst_name": "ハワイ",
+      "dst_code": "HGS",
+      "country_name": "ハワイ",
+      "country_code": "HWI",
+      "city_name": "ホノルル",
+      "city_code": "HNL",
+      "マップコード": "HWI"
+    },
+    {
+      "dst_name": "ハワイ",
+      "dst_code": "HGS",
+      "country_name": "ハワイ",
+      "country_code": "HWI",
+      "city_name": "ハワイ島",
+      "city_code": "KOA",
+      "マップコード": "HWI"
+    },
+    {
+      "dst_name": "ハワイ",
+      "dst_code": "HGS",
+      "country_name": "ハワイ",
+      "country_code": "HWI",
+      "city_name": "カウアイ島",
+      "city_code": "JIH",
+      "マップコード": "HWI"
+    },
+    {
+      "dst_name": "ハワイ",
+      "dst_code": "HGS",
+      "country_name": "ハワイ",
+      "country_code": "HWI",
+      "city_name": "ラナイ島",
+      "city_code": "LNY",
+      "マップコード": "HWI"
+    },
+    {
+      "dst_name": "グアム・サイパン",
+      "dst_code": "HGS",
+      "country_name": "グアム",
+      "country_code": "GUM",
+      "city_name": "グアム",
+      "city_code": "GUM"
+    },
+    {
+      "dst_name": "グアム・サイパン",
+      "dst_code": "HGS",
+      "country_name": "サイパン",
+      "country_code": "SPN",
+      "city_name": "サイパン",
+      "city_code": "SPN"
+    }
+  ],
+  "EUR": [
+    {
+      "dst_name": "ヨーロッパ",
+      "dst_code": "EUR",
+      "country_name": "フランス",
+      "country_code": "FRA",
+      "city_name": "パリ",
+      "city_code": "PAR",
+      "マップコード": "EUR"
+    },
+    {
+      "dst_name": "ヨーロッパ",
+      "dst_code": "EUR",
+      "country_name": "フランス",
+      "country_code": "FRA",
+      "city_name": "マルセイユ",
+      "city_code": "MRS",
+      "マップコード": "EUR"
+    },
+    {
+      "dst_name": "ヨーロッパ",
+      "dst_code": "EUR",
+      "country_name": "イタリア",
+      "country_code": "ITA",
+      "city_name": "ローマ",
+      "city_code": "ROM",
+      "マップコード": "EUR"
+    },
+    {
+      "dst_name": "ヨーロッパ",
+      "dst_code": "EUR",
+      "country_name": "イタリア",
+      "country_code": "ITA",
+      "city_name": "フィレンツェ",
+      "city_code": "FLR",
+      "マップコード": "EUR"
+    }
+  ],
+  "AFR": [
+    {
+      "dst_name": "中近東・アフリカ",
+      "dst_code": "AFR",
+      "country_name": "モロッコ",
+      "country_code": "MAR",
+      "city_name": "カサブランカ",
+      "city_code": "CAS",
+      "マップコード": "AFR"
+    }
+  ],
+  "OCE": [
+    {
+      "dst_name": "南太平洋・オセアニア",
+      "dst_code": "OCE",
+      "country_name": "オーストラリア",
+      "country_code": "AUS",
+      "city_name": "ケアンズ",
+      "city_code": "CNS",
+      "マップコード": "OCE"
+    },
+    {
+      "dst_name": "南太平洋・オセアニア",
+      "dst_code": "OCE",
+      "country_name": "ニュージーランド",
+      "country_code": "NZL",
+      "city_name": "クライストチャーチ",
+      "city_code": "CHC",
+      "マップコード": "OCE"
+    }
+  ],
+  "CAN": [
+    {
+      "dst_name": "カナダ",
+      "dst_code": "CAN",
+      "country_name": "カナダ",
+      "country_code": "CAN",
+      "city_name": "イエローナイフ",
+      "city_code": "YZF",
+      "マップコード": "CAN"
+    }
+  ],
+  "USA": [
+    {
+      "dst_name": "アメリカ",
+      "dst_code": "USA",
+      "country_name": "アメリカ合衆国",
+      "country_code": "USA",
+      "city_name": "ニューヨーク",
+      "city_code": "NYC",
+      "マップコード": "USA"
+    }
+  ],
+  "MEX": [
+    {
+      "dst_name": "中南米・カリブ海",
+      "dst_code": "MEX",
+      "country_name": "メキシコ",
+      "country_code": "MEX",
+      "city_name": "メキシコシティ",
+      "city_code": "MWX",
+      "マップコード": "MEX"
+    },
+    {
+      "dst_name": "中南米・カリブ海",
+      "dst_code": "MEX",
+      "country_name": "メキシコ",
+      "country_code": "MEX",
+      "city_name": "カンクン",
+      "city_code": "CUN",
+      "マップコード": "MEX"
+    }
+  ]
+};
+
+console.log(sortByDst['ASI']);
---
+++ dist/js/test2.js
@@ -0,0 +1,122 @@
+$(function () {
+  Vue.component('city-list', {
+    props: ['city'],
+    template: `
+    <ul v-bind:class="city.country_code">
+      <li>{{ city.city_name }}</li>
+    </ul>
+  `
+  })
+  new Vue({
+    el: '#app',
+    data() {
+      return {
+        // modal
+        checkedDst: 'ASI',
+        checkedCountry: '',
+        checkedCity: '',
+        modal1: false,
+        // data
+        cityResult: [],
+        mapList: [],
+        sortByDst: [],
+        sortByCountry: [],
+        // results
+        dstResult: [],
+        countryResult: [],
+        country_city_isMerged: [],
+        show: true
+      }
+    },
+    created: function () {
+      axios.get('../modal/data/map2.csv').then(function (response) {
+        // 取得完了したらlistリストに代入
+        var tmpArray = response.data.split("\n");
+        var data_head = [], data_body = [];
+        for (var i = 0; i < tmpArray.length; i++) {
+          data_body[i] = tmpArray[i].split(",");
+        }
+        data_head = data_body[0];
+        data_body = data_body.slice(1)
+        this.mapList = this.toJson(data_head, data_body)
+        this.sortByDst = this.groupBy(this.mapList, 'dst_code')
+        this.sortByCountry = this.groupBy(this.mapList, 'country_name')
+      }.bind(this)).catch(function (e) {
+        console.error(e)
+      })
+    },
+    methods: {
+      toJson: function (header, body) {
+        var json = body.map(function (row) {
+          return row.reduce(function (accum, currentValue, i) {
+            accum[header[i]] = currentValue;
+            return accum;
+          }, {});
+        });
+        return json;
+      },
+      groupBy: function (objectArray, property) {
+        return objectArray.reduce(function (acc, obj) {
+          var key = obj[property];
+          if (!acc[key]) {
+            acc[key] = [];
+          }
+          acc[key].push(obj);
+          return acc;
+        }, {});
+      },
+      closeModal: function () {
+        // defaultの状態に戻したい
+        this.checkedDst = '',
+        this.checkedCountry = '',
+        this.checkedCity = ''
+      }
+    },
+    computed: {
+      matchedDst_returnCountry: function () {
+        var _mapList = this.mapList;
+        var _checkedDst = this.checkedDst
+        var dst_result = _mapList.filter(function (item, index) {
+          if (item.dst_code === _checkedDst) return true;
+        });
+        this.dstResult = dst_result
+
+        var arrObj = {};
+        for (var i = 0; i < dst_result.length; i++) {
+          arrObj[dst_result[i]['country_name']] = dst_result[i];
+        }
+        country_result = [];
+        for (var key in arrObj) {
+          country_result.push(arrObj[key]);
+        }
+
+        this.countryResult = country_result
+        return country_result
+      },
+      matchedCountry_returnCity: function () {
+        var _mapList = this.mapList;
+        var _checkedCountry = this.checkedCountry
+        var city_result = _mapList.filter(function (item, index) {
+          if (item.country_code === _checkedCountry) return true;
+        });
+        this.cityResult = city_result
+        return city_result
+      }
+    },
+    watch: {
+      checkedCountry: function() {
+        this.$nextTick(function () {
+          $(this.$el).find('.city-parent').find('ul').css('display', 'none');
+          $(this.$el).find('#' + this.checkedCountry).find('ul').css('display', 'block');
+        })
+      },
+      checkedDst: function () {
+        this.$nextTick(function () {
+          $(this.$el).find('.city-parent').find('ul').css('display', 'none');
+          $(this.$el).find('#' + this.checkedCountry).find('ul').css('display', 'block');
+        })
+      }
+    }
+  });
+});
+
